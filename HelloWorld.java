cebbde74f9ec3cd71306c7be977b5719e36ab69a
KimInsu01 <insu@sogang.ac.kr>
2019-07-16 16:47:22 +0900

    2019-07-16 New HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+//파일 새로만들기 => ctrl + n
+//편집기 확장 => ctrl + m
+//한줄 복사후 아래로 붙여넣기 => ctrl + alt + down arrow
+//한줄 지우기 => ctel + d
+//한줄 이동 => ctrl + 방향키
+//실행 => ctrl + F11
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+	}
+}
